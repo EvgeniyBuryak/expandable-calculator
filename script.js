85c0b70f7ae237b82b73bb37e0cfc7ca805c8082
Jhonah <buryakone@gmail.com>
2021-06-22 17:54:50 +0700

    добавлена возможность расширить калькулятор новыми простыми операциями

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-let calc = new Calculator;
+let powerCalc = new Calculator;
 
-console.log(calc.calculate("3 + 7")); // 10
-console.log(calc.calculate("16 - 7")); // 9
-console.log(calc);
+console.log(powerCalc.calculate("3 + 7"));  // 10
+console.log(powerCalc.calculate("16 - 7")); // 9
 
 function Calculator() {
     this.a;
     this.b;
-    this.operator;
+    //this.operator;
     this.calculate = function(str) {
 
         let arr = str.split(" ");
         this.a = +arr[0];
         this.b = +arr[2];
 
-        this.operator = arr[1];
+        let operator = arr[1];
 
-        switch (this.operator) {
+        switch (operator) {
             case "+":
                 return this.a + this.b;
             case "-":
                 return this.a - this.b;
+            case "*":
+                return this["*"](this.a, this.b);
             default:
                 break;
         }
     }
+    this.addMethod = function (operator, func) {
+        this[operator] = func;
+    }
 }
+
+powerCalc.addMethod("*", (a, b) => a * b);
+//powerCalc.addMethod("/", (a, b) => a / b);
+//powerCalc.addMethod("**", (a, b) => a ** b);
+
+let result = powerCalc.calculate("3 * 8"); // 24
+console.log(`Результат: ${result}`);
+
+console.log(powerCalc);
