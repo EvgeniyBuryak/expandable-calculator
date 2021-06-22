d907bbd0cdbd1b6935a2fe3f987cc6136d70b51a
Jhonah <buryakone@gmail.com>
2021-06-22 23:14:20 +0700

    добавил проверку на ошибкиа корректнхе чиела и оператоар

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let powerCalc = new Calculator;
 
-console.log(powerCalc.calculate("3 + 7"));  // 10
-console.log(powerCalc.calculate("16 - 7")); // 9
+console.log(powerCalc.calculate("3 +f 7"));  // "Такой оператор не определен!"
+console.log(powerCalc.calculate("136 - 79")); // 9
 
-function Calculator() {
-    //this.a;
-    //this.b;
-    //this.operator;
-    this.calculate = function(str) {
+function Calculator() {    
+
+    this.calculate = function (str) {
 
         let arr = str.split(" ");
         this.a = +arr[0];
         this.b = +arr[2];
+
+        if (isNaN(this.a) || isNaN(this.b)) return "Входящие значения не корректны!";
 
         let operator = arr[1];
 
@@ -28,8 +28,14 @@ function Calculator() {
         /*
          * Ниже обработка операций добавленных
          * с помощью addMethod
+         *
+         * .? - опциональная цепочка, если не найдено
+         * свойства объекта, возвращает "undefined"
+         *
+         * ?? - проверяет на "undefined", если да,
+         * выполняется творое выражение
          * */
-        return this[operator](this.a, this.b);
+        return this[operator]?.(this.a, this.b) ?? "Такой оператор не определен!";
     }
     this.addMethod = function (operator, func) {
         this[operator] = func;
@@ -40,7 +46,7 @@ powerCalc.addMethod("*", (a, b) => a * b);
 powerCalc.addMethod("/", (a, b) => a / b);
 powerCalc.addMethod("**", (a, b) => a ** b);
 
-let result = powerCalc.calculate("25 / 5"); // 24
+let result = powerCalc.calculate("25 / 5"); // 5
 console.log(`Результат: ${result}`);
 
-console.log(powerCalc);
+console.log(powerCalc.calculate("3 * 3u")); // "Входящие значения не корректны!"
